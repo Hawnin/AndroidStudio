92bb5efe8cf97c69e6c088067cf6329fb1de3338
Hawnin <hawnin@gmail.com>
2019-08-10 09:53:02 +0300

    added secondary fragment components

---
+++ FragmentRunTime/app/src/main/java/com/example/fragmentruntime/SecondaryFragment.java
@@ -0,0 +1,31 @@
+package com.example.fragmentruntime;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SecondaryFragment extends Fragment {
+
+
+    public SecondaryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_secondary, container, false);
+    }
+
+}
